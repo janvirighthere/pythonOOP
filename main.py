c9f8706f74a01d746fbf0980813bf18dea6850fa
unknown <janvier.bur@gmail.com>
2021-02-04 12:00:26 -0500

    Add geometry.py

--- main.py
+++ main.py
@@ -1,1 +1,20 @@
 # Dunder Methods
+class Book():
+
+    def __init__(self, title, author, pages):
+        self.title = title
+        self.author = author
+        self.pages = pages
+
+    def __str__(self):
+        return f"{self.title} by {self.author}."
+    
+    def __len__(self):
+        return self.pages
+
+    def __del__(self):
+        print("Book was deleted")
+
+b = Book("Pythons rocks", "janvier", 3080)
+del(b)
+print(b)
