b83e2f1cbb188f74036923fbc3b08a14b260fb0f
unknown <janvier.bur@gmail.com>
2021-02-04 11:36:10 -0500

    Polymorphism

--- main.py
+++ main.py
@@ -1,16 +1,26 @@
-class Circle():
-
-    PI = 3.14
-
-    def __init__(self,radius=1):
-        self.radius = radius
-
-    def get_circumference(self):
-        return self.radius * Circle.PI * 2
-
-    def get_area(self):
-        return self.radius * self.radius * Circle.PI
+class Animal():
     
-c = Circle(30)
-print(c.get_circumference())
-print(c.get_area())
+    def __init__(self):
+        print("Animal Created")
+    
+    def whom_am_i(self):
+        print("I am an animal")
+    
+    def eat(self):
+        print("I am eating")
+
+class Dog(Animal):
+
+    def __init__(self):
+        super().__init__()
+        print("Dog Created")
+
+    def whom_am_i(self):
+        print("I am a dog")
+    
+    def eat(self):
+        print("I am a dog and eating")
+
+dog = Dog()
+dog.eat()
+dog.whom_am_i()
